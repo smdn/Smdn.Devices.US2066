dfe8224cefc967f264b764612e54182f54a91fee
smdn <smdn@smdn.jp>
2021-12-18 20:41:08 +0900

    use file-scoped namespace declaration

--- misc/generate-characterbitmap-table/CGRomBitmap.cs
+++ misc/generate-characterbitmap-table/CGRomBitmap.cs
@@ -1,13 +1,11 @@
 // SPDX-FileCopyrightText: 2021 smdn <smdn@smdn.jp>
 // SPDX-License-Identifier: MIT
-
-using System;
 using System.Collections.Generic;
 
-namespace Smdn.Devices.US2066 {
-  internal partial class CGRomBitmap {
-    public static IReadOnlyList<byte> GetBitmapCGRomA(byte by) => bitmapCGROM_A[by];
-    public static IReadOnlyList<byte> GetBitmapCGRomB(byte by) => bitmapCGROM_B[by];
-    public static IReadOnlyList<byte> GetBitmapCGRomC(byte by) => bitmapCGROM_C[by];
-  }
+namespace Smdn.Devices.US2066;
+
+internal partial class CGRomBitmap {
+  public static IReadOnlyList<byte> GetBitmapCGRomA(byte by) => bitmapCGROM_A[by];
+  public static IReadOnlyList<byte> GetBitmapCGRomB(byte by) => bitmapCGROM_B[by];
+  public static IReadOnlyList<byte> GetBitmapCGRomC(byte by) => bitmapCGROM_C[by];
 }
