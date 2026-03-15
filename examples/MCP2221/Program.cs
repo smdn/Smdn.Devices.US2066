6df045e363b7c2483bb5ead07b0c674848fd2592
smdn <smdn@smdn.jp>
2026-03-15 20:04:03 +0900

    update to use Smdn.Devices.Mcp2221A version 1.0.0-preview1

--- examples/MCP2221/Program.cs
+++ examples/MCP2221/Program.cs
@@ -3,15 +3,20 @@
 
 using System;
 
-using Smdn.Devices.MCP2221;
-using Smdn.Devices.MCP2221.GpioAdapter;
-using Smdn.Devices.US2066;
+using Microsoft.Extensions.DependencyInjection;
 
-using var mcp2221 = MCP2221.Open();
+using Smdn.Devices.Mcp2221A;
+using Smdn.Devices.US2066;
+using Smdn.IO.UsbHid.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddHidSharpUsbHid();
+
+using var serviceProvider = services.BuildServiceProvider();
+using var mcp2221a = Mcp2221A.Create(serviceProvider);
 using var display = SO1602A.Create(
-  new MCP2221I2cDevice(mcp2221.I2C, SO1602A.DefaultI2CAddress) {
-    BusSpeed = I2CBusSpeed.FastMode
-  }
+  mcp2221a.I2c.CreateDevice(SO1602A.DefaultI2CAddress).WithFastMode()
 );
 
 display.Write("Hello, world!");
